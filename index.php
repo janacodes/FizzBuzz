bfef776148815abbd59b65e9710986c8c5e0baba
janacodes <jana_wendt@gmx.net>
2020-02-07 12:35:11 +0100

    solved fizzbuzz

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+for ($x = 1; $x <= 100; $x++) {
+  if ($x % 3 == 0) {
+    if ($x % 5 == 0) {
+    echo "FizzBuzz" . "<br>";
+  }
+  else {
+    echo "Fizz" . "<br>";
+  }
+}
+else if ($x % 5 == 0){
+  echo "Buzz" . "<br>";
+}
+else {
+  echo $x . "<br>";
+}
+}
+?>
